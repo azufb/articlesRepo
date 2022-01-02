1df5207642a1c4b9bb31a1584c04df2f8ed08213
azufb <curiousazunas@gmail.com>
2022-01-02 23:43:23 +0900

    Number()、parseInt()、parseFloat()の3種検証

---
+++ 20220104/index.js
@@ -0,0 +1,38 @@
+let sampleNum = '12345';
+console.log(typeof sampleNum);
+
+let samplePoint = '123.5';
+console.log(typeof samplePoint);
+
+let sampleStr = 'あいうえお';
+console.log(typeof sampleStr);
+
+// Number()メソッド
+console.log(Number(sampleNum));
+console.log(typeof Number(sampleNum));
+
+console.log(Number(samplePoint));
+console.log(typeof Number(samplePoint));
+
+console.log(Number(sampleStr));
+console.log(typeof Number(sampleStr));
+
+// parseInt()メソッド
+console.log(parseInt(sampleNum));
+console.log(typeof parseInt(sampleNum));
+
+console.log(parseInt(samplePoint));
+console.log(typeof parseInt(samplePoint));
+
+console.log(parseInt(sampleStr));
+console.log(typeof parseInt(sampleStr));
+
+// parseFloat()メソッド
+console.log(parseFloat(sampleNum));
+console.log(typeof parseFloat(sampleNum));
+
+console.log(parseFloat(samplePoint));
+console.log(typeof parseFloat(samplePoint));
+
+console.log(parseFloat(sampleStr));
+console.log(typeof parseFloat(sampleStr));
